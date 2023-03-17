b67b0c3572c16e860d04a67e2d3164691ee51062
manh.nguyen <manh.nguyen@vnresource.org>
2023-03-17 11:34:58 +0700

    First commit

---
+++ GIT/GIT/MN.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace GIT
+{
+    partial class MN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNameBranch = new System.Windows.Forms.TextBox();
+            this.btnCreateBranch = new System.Windows.Forms.Button();
+            this.txtBranchCherry = new System.Windows.Forms.TextBox();
+            this.btnBranchCherry = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtNameBranch
+            // 
+            this.txtNameBranch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNameBranch.Location = new System.Drawing.Point(54, 43);
+            this.txtNameBranch.Name = "txtNameBranch";
+            this.txtNameBranch.Size = new System.Drawing.Size(435, 22);
+            this.txtNameBranch.TabIndex = 0;
+            // 
+            // btnCreateBranch
+            // 
+            this.btnCreateBranch.BackColor = System.Drawing.Color.Transparent;
+            this.btnCreateBranch.ForeColor = System.Drawing.Color.Black;
+            this.btnCreateBranch.Location = new System.Drawing.Point(208, 155);
+            this.btnCreateBranch.Name = "btnCreateBranch";
+            this.btnCreateBranch.Size = new System.Drawing.Size(131, 28);
+            this.btnCreateBranch.TabIndex = 1;
+            this.btnCreateBranch.Text = "Tạo nhánh";
+            this.btnCreateBranch.UseVisualStyleBackColor = false;
+            this.btnCreateBranch.Click += new System.EventHandler(this.btnCreateBranch_Click);
+            // 
+            // txtBranchCherry
+            // 
+            this.txtBranchCherry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBranchCherry.Location = new System.Drawing.Point(54, 98);
+            this.txtBranchCherry.Name = "txtBranchCherry";
+            this.txtBranchCherry.Size = new System.Drawing.Size(435, 21);
+            this.txtBranchCherry.TabIndex = 2;
+            // 
+            // btnBranchCherry
+            // 
+            this.btnBranchCherry.Location = new System.Drawing.Point(208, 211);
+            this.btnBranchCherry.Name = "btnBranchCherry";
+            this.btnBranchCherry.Size = new System.Drawing.Size(131, 28);
+            this.btnBranchCherry.TabIndex = 3;
+            this.btnBranchCherry.Text = "Tạo nhánh cherry";
+            this.btnBranchCherry.UseVisualStyleBackColor = true;
+            this.btnBranchCherry.Click += new System.EventHandler(this.btnBranchCherry_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(420, 141);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // MN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 262);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnBranchCherry);
+            this.Controls.Add(this.txtBranchCherry);
+            this.Controls.Add(this.btnCreateBranch);
+            this.Controls.Add(this.txtNameBranch);
+            this.Name = "MN";
+            this.Text = "MN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNameBranch;
+        private System.Windows.Forms.Button btnCreateBranch;
+        private System.Windows.Forms.TextBox txtBranchCherry;
+        private System.Windows.Forms.Button btnBranchCherry;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ GIT/GIT/MN.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace GIT
+{
+    public partial class MN : Form
+    {
+        public MN()
+        {
+            InitializeComponent();
+        }
+        private string strUserName = ConfigurationManager.AppSettings["UserName"];
+        private string strFolder = ConfigurationManager.AppSettings["Folder"];
+        private string strTitle = string.Empty;
+
+        public object StandardCharsets { get; private set; }
+
+        private void btnCreateBranch_Click(object sender, EventArgs e)
+        {
+            GetDateTime(ref strTitle);
+            txtNameBranch.Text = strTitle + "-";
+            Clipboard.SetText(txtNameBranch.Text);
+            string a = "{\"merchantId\":\"{MerchantId}\",\"reqData\":\"{reqData}\"}";
+            string c = "{" + "MerchantId" + "}";
+            string b = a.Replace(c, "1222222222");
+
+        }
+        private void btnBranchCherry_Click(object sender, EventArgs e)
+        {
+            GetDateTime(ref strTitle);
+            txtBranchCherry.Text = strTitle + "-Cherry-";
+            Clipboard.SetText(txtBranchCherry.Text);
+            string str = "{\"serviceName\":\"ADD_ORDER_INFOR\",\"orderId\":\"o1\",\"posId\":\"p1\", \"amount\":\"1000\", \"description\" : \"mo ta 1\" }";
+            string key = "0123456789ABCDEF";
+            string strEn = EncryptAES128(key, str);
+            string strDe = DecryptAES128(key, strEn);
+
+            var date1 = new DateTime(2022, 7, 15);
+            var date2 = new DateTime(2023, 10, 13);
+            var diff = date2- date1;
+
+        }
+        private void GetDateTime(ref string strTitle)
+        {
+            string strGetDay = DateTime.Now.ToString("dd-MM");
+            string strGetTime = DateTime.Now.ToString("HH.mm.ss");
+            strTitle = strFolder + "/" + strUserName + "/Day." + strGetDay + ".Time." + strGetTime;
+        }
+        public static string EncryptAES128(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                aes.IV = iv;
+                aes.Mode = CipherMode.ECB;
+                aes.BlockSize = 128;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+        public static string DecryptAES128(string key, string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                aes.IV = iv;
+                aes.Mode = CipherMode.ECB;
+                aes.BlockSize = 128;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
